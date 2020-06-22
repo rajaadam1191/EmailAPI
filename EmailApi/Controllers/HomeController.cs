92237a4f9295b1da5da05184ac26730eb6d615fc
ManikandanB2Q <manikandan.r@binary2quantum.com>
2020-06-22 19:15:02 +0530

    after email using smtp done

--- EmailApi/Controllers/HomeController.cs
+++ EmailApi/Controllers/HomeController.cs
@@ -7,11 +7,13 @@ using System.Configuration;
 using System.Web.Http;
 using EmailApi.Models;
 using EmailApi.Handler;
- 
+
 using System.Net.Mail;
 using System.Net.Configuration;
 using System.IO;
 using System.Diagnostics;
+using System.Web.Mail;
+
 namespace EmailApi.Controllers
 {
  
@@ -45,7 +47,7 @@ namespace EmailApi.Controllers
             info.smtp = smtp.Network.Host;
             info.port = smtp.Network.Port;
             info.EnableSsl = smtp.Network.EnableSsl;
-            MailMessage mm = new MailMessage(info.UserName, "manikandan.r@binary2quantum.com");
+            System.Net.Mail.MailMessage mm = new System.Net.Mail.MailMessage(info.UserName, "manikandan.r@binary2quantum.com");
             //mm.CC.Add("manikandan.r@binary2quantum.com,manikandan.ramsaro@gmail.com"); -- example for CC
             mm.Subject = "Test Email using Web APi";
             mm.Body = string.Format("Dear {0},<br /><br />Test email from Dot NEt web api", "Mani");
@@ -77,6 +79,42 @@ namespace EmailApi.Controllers
           
                 
              
+            return info;
+        }
+        [HttpGet]
+        public EmailDatas sendUsingIP()
+        {
+            EmailDatas info = new EmailDatas();
+
+            SmtpSection smtp = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
+            info.UserName = smtp.Network.UserName;
+            info.password = smtp.Network.Password;
+            info.smtp = smtp.Network.Host;
+            info.port = smtp.Network.Port;
+            info.EnableSsl = smtp.Network.EnableSsl;
+            try
+            {
+                System.Web.Mail.MailMessage Msg = new System.Web.Mail.MailMessage();
+                Msg.From = info.UserName;
+                // Recipient e-mail address.
+                Msg.To = "manikandan.r@binary2quantum.com";
+                Msg.Subject = "Test email from Poclain";
+                Msg.Body = "Test email sent without Password";
+                // your remote SMTP server IP.
+                SmtpMail.SmtpServer = info.smtp;//your ip address
+                SmtpMail.Send(Msg);
+                Msg = null;
+                action.WriteEntry("Test email sent to manikandan.r@binary2quantum.com", "Good", "HomeController");
+            }
+            catch (Exception ex)
+            {
+                info.status = "Something went wrong while sending email !";
+                info.errorInfo = ex.ToString();
+                action.WriteEntry(ex.ToString(), "Error", "HomeController");
+            }
+
+
+
             return info;
         }
     }
